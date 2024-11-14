27b7392a21cad30a5887aa57715285d1b52eceeb
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-11-14 21:22:43 +0100

    feat: enhance dog listing with detailed information
    Update DogItemListing to display detailed dog data including age, 
    sex, and characteristics. Modify dogData to include new fields. 
    Implement characteristic icons for better visual representation. 
    This improves user experience by providing more relevant 
    information about each dog.

--- components/dogListing/dog-listing.tsx
+++ components/dogListing/dog-listing.tsx
@@ -7,16 +7,25 @@ const dogData = [
     id: 1,
     name: 'Dog 1',
     image: 'https://picsum.photos/300',
+    age: 10,
+    sex: 1,
+    caracteristics: [0, 3, 8, 10],
   },
   {
     id: 2,
     name: 'Dog 2',
     image: 'https://picsum.photos/300',
+    age: 7,
+    sex: 1,
+    caracteristics: [0, 3, 8, 10],
   },
   {
     id: 3,
     name: 'Dog 3',
     image: 'https://picsum.photos/300',
+    age: 4,
+    sex: 0,
+    caracteristics: [0, 3, 8, 10],
   },
 ];
 
@@ -24,7 +33,7 @@ export default function DogListing() {
   return (
     <FlashList
       data={dogData}
-      renderItem={({ item }) => <DogItemListing />}
+      renderItem={({ item }) => <DogItemListing dogCardData={item} />}
       estimatedItemSize={10}
       ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
       contentContainerStyle={{ paddingHorizontal: 20, paddingBottom: 80 }}
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components/native';
 
 const DogCard = styled.View`
-  background-color: #000;
   border-radius: 20px;
   flex: 1;
   height: 331px;
@@ -9,6 +8,8 @@ const DogCard = styled.View`
   display: flex;
   justify-content: flex-end;
   align-items: flex-start;
+  overflow: hidden;
+  gap: 8px;
 `;
 
 export { DogCard };
