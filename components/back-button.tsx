b783cabcf2f6b03bbb2349772c93ab7e3dd3bacb
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-11-15 23:50:46 +0100

    feat: add parallax scroll view and back button component
    Introduce a new `ParallaxScrollView` component for enhanced 
    scrolling effects and a `BackButton` component for navigation. 
    Remove unused navigation dependency and update the DogDetails 
    screen to utilize the new components, improving user 
    experience and interface consistency.

---
+++ components/back-button.tsx
@@ -0,0 +1,14 @@
+import { Entypo } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { BackButton } from "./ui/button";
+
+export default function Back() {
+  const { top } = useSafeAreaInsets();
+
+  return (
+    <BackButton onPress={() => router.back()} style={{ marginTop: top }}>
+      <Entypo name="chevron-left" size={13} color="white" />
+    </BackButton>
+  )
+}
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,4 +9,19 @@ const StyledButton = styled.Pressable`
   color: #fff;
 `;
 
-export default StyledButton;
+const BackButton = styled.Pressable`
+  paddingVertical: 10px;
+  paddingHorizontal: 12px;
+  position: absolute;
+  left: 25px;
+  background-color: #F7A400;
+  border-radius: 1000px;
+  width: 32px;
+  height: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 100;
+`;
+
+export { BackButton, StyledButton };
