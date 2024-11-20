ecfec1bb58746538b1816743fe9c6a7b434b25b5
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-11-20 13:25:17 +0100

    feat: add settings screen and update account navigation
    Implement a new Settings screen with a back button and integrate it 
    into the account tab. Update the Account screen to include a 
    icon that navigates to the Settings screen. Use safe area insets 
    for proper layout on different devices.

--- app/(auth)/(tabs)/(account)/account.tsx
+++ app/(auth)/(tabs)/(account)/account.tsx
@@ -1,10 +1,26 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "react-native";
+import RoundedIconLink from "@/components/rounded-icon-link";
+import { Body } from "@/components/ui/text";
+import { FontAwesome } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function AccountScreen() {
+    const insets = useSafeAreaInsets();
+
     return (
-        <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>AccountScreen</Text>
-        </SafeAreaView>
+        <View style={{ flex: 1, paddingTop: insets.top, backgroundColor: 'white' }}>
+            <View>
+                <View style={{ paddingHorizontal: 20, display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', gap: 12 }}>
+                    <RoundedIconLink
+                        icon={<FontAwesome name="gear" size={20} color="white" />}
+                        onPress={() => router.push('/settings')}
+                    />
+                </View>
+            </View>
+            <View>
+                <Body>Account</Body>
+            </View>
+        </View>
     );
 }
--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -8,6 +8,7 @@ export default function AuthLayout() {
       <Stack.Screen name="(tabs)" />
       <Stack.Screen name="messages" />
       <Stack.Screen name="notifications" />
+      <Stack.Screen name="settings" />
     </Stack>
   )
 }
