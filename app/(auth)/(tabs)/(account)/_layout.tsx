9447a2719cade5c40c9dd6e3045e4252aa7cc17b
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-11-21 14:10:10 +0100

    implement supabase auth and expo secure store

--- app/(auth)/(tabs)/(account)/_layout.tsx
+++ app/(auth)/(tabs)/(account)/_layout.tsx
@@ -4,6 +4,7 @@ export default function AccountLayout() {
     return (
         <Stack>
             <Stack.Screen name="account" options={{ headerShown: false }} />
+            <Stack.Screen name="settings" options={{ headerShown: false }} />
         </Stack>
     );
 }
