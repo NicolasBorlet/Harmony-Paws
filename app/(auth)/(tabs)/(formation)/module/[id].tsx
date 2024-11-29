8cb74621f8a22e5892d593cd20f9ad6610161260
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-11-29 15:20:12 +0100

    refactor: update module layout and navigation options
    Remove modal presentation from the module screen and adjust the layout 
    of the module component. Implement safe area insets for better 
    padding and add a back button with a navigation title. This improves 
    the user experience by ensuring proper spacing and navigation 
    functionality.

--- app/(auth)/(tabs)/(formation)/module/[id].tsx
+++ app/(auth)/(tabs)/(formation)/module/[id].tsx
@@ -1,10 +1,27 @@
-import { Body } from "@/components/ui/text";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { i18n } from "@/app/_layout";
+import Back from "@/components/back-button";
+import { NavigationTitle } from "@/components/ui/text";
+import { View } from "react-native";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Module () {
+  const insets = useSafeAreaInsets();
+  
   return (
-    <SafeAreaView>
-      <Body>Module</Body>
+    <SafeAreaView style={{ paddingHorizontal: 20, paddingTop: 32, flex: 1, paddingBottom: insets.bottom, gap: 48 }}>
+      <View style={{ gap: 24, display: "flex", flexDirection: "column" }}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 10,
+            alignItems: "center",
+        }}
+        >
+          <Back position="relative" left="0" />
+          <NavigationTitle color="#000">{i18n.t('joinTheRide')}</NavigationTitle>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
--- app/(auth)/(tabs)/(formation)/module/_layout.tsx
+++ app/(auth)/(tabs)/(formation)/module/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function FormationLayout() {
     return (
         <Stack>
-            <Stack.Screen name="[id]" options={{ headerShown: false, presentation: "modal" }} />
+            <Stack.Screen name="[id]" options={{ headerShown: false }} />
         </Stack>
     );
 }
