80f0a2b28661665fb2080f9a7a236d1ec53da980
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-12-06 11:32:08 +0100

    feat(onboarding): Implement new onboarding experience with animated scrolling and pagination

--- app/(auth)/onboarding.tsx
+++ app/(auth)/onboarding.tsx
@@ -1,30 +1,152 @@
-import { Body } from '@/components/ui/text'
-import { router } from 'expo-router'
-import { useEffect } from 'react'
-import { Button, StyleSheet, View } from 'react-native'
-import { MMKV } from 'react-native-mmkv'
+
+import CustomButton from '@/components/onboarding/CustomBottom';
+import Pagination from '@/components/onboarding/Pagination';
+import { Body } from '@/components/ui/text';
+import { StyleSheet, useWindowDimensions, View } from 'react-native';
+import { MMKV } from 'react-native-mmkv';
+import Animated, { Extrapolation, interpolate, useAnimatedRef, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 // Initialize MMKV
 export const storage = new MMKV()
 
+const data = [
+  {
+    id: 1,
+    image: require('../../assets/images/image1.png'),
+    title: 'Lorem Ipsum',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 2,
+    image: require('../../assets/images/image2.png'),
+    title: 'Lorem Ipsum',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 3,
+    image: require('../../assets/images/image3.png'),
+    title: 'Lorem Ipsum',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+];
+
 export default function OnBoarding() {
   const onboarding = storage.getBoolean('onBoarding')
 
-  useEffect(() => {
-    console.log('Onboarding', onboarding)
-  }, [onboarding])
+  const { width: SCREEN_WIDTH } = useWindowDimensions();
+  const flatListRef = useAnimatedRef(null);
+  const x = useSharedValue(0);
+  const flatListIndex = useSharedValue(0);
+
+  const onViewableItemsChanged = ({ viewableItems }) => {
+    flatListIndex.value = viewableItems[0].index;
+  };
+
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: event => {
+      x.value = event.contentOffset.x;
+    },
+  });
+
+  // eslint-disable-next-line react/no-unstable-nested-components
+  const RenderItem = ({ item, index }) => {
+    const imageAnimationStyle = useAnimatedStyle(() => {
+      const opacityAnimation = interpolate(
+        x.value,
+        [
+          (index - 1) * SCREEN_WIDTH,
+          index * SCREEN_WIDTH,
+          (index + 1) * SCREEN_WIDTH,
+        ],
+        [0, 1, 0],
+        Extrapolation.CLAMP,
+      );
+      const translateYAnimation = interpolate(
+        x.value,
+        [
+          (index - 1) * SCREEN_WIDTH,
+          index * SCREEN_WIDTH,
+          (index + 1) * SCREEN_WIDTH,
+        ],
+        [100, 0, 100],
+        Extrapolation.CLAMP,
+      );
+      return {
+        opacity: opacityAnimation,
+        width: SCREEN_WIDTH * 0.8,
+        height: SCREEN_WIDTH * 0.8,
+        transform: [{ translateY: translateYAnimation }],
+      };
+    });
+    const textAnimationStyle = useAnimatedStyle(() => {
+      const opacityAnimation = interpolate(
+        x.value,
+        [
+          (index - 1) * SCREEN_WIDTH,
+          index * SCREEN_WIDTH,
+          (index + 1) * SCREEN_WIDTH,
+        ],
+        [0, 1, 0],
+        Extrapolation.CLAMP,
+      );
+      const translateYAnimation = interpolate(
+        x.value,
+        [
+          (index - 1) * SCREEN_WIDTH,
+          index * SCREEN_WIDTH,
+          (index + 1) * SCREEN_WIDTH,
+        ],
+        [100, 0, 100],
+        Extrapolation.CLAMP,
+      );
+
+      return {
+        opacity: opacityAnimation,
+        transform: [{ translateY: translateYAnimation }],
+      };
+    });
+    return (
+      <View style={[styles.itemContainer, { width: SCREEN_WIDTH }]}>
+        <Animated.Image source={item.image} style={imageAnimationStyle} />
+        <Animated.View style={textAnimationStyle}>
+          <Body style={styles.itemTitle}>{item.title}</Body>
+          <Body style={styles.itemText}>{item.text}</Body>
+        </Animated.View>
+      </View>
+    );
+  };
 
   return (
-    <View style={styles.container}>
-      <Body>Onboarding</Body>
-      <Button
-        title='set onBoarding'
-        onPress={() => {
-          storage.set('onBoarding', true)
-          router.replace('/(auth)/(tabs)/(home)')
+    <SafeAreaView style={styles.container}>
+      <Animated.FlatList
+        ref={flatListRef}
+        onScroll={onScroll}
+        data={data}
+        renderItem={({ item, index }) => {
+          return <RenderItem item={item} index={index} />;
+        }}
+        keyExtractor={item => item.id.toString()}
+        scrollEventThrottle={16}
+        horizontal={true}
+        bounces={false}
+        pagingEnabled={true}
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={onViewableItemsChanged}
+        viewabilityConfig={{
+          minimumViewTime: 300,
+          viewAreaCoveragePercentThreshold: 10,
         }}
       />
-    </View>
+      <View style={styles.bottomContainer}>
+        <Pagination data={data} x={x} screenWidth={SCREEN_WIDTH} />
+        <CustomButton
+          flatListRef={flatListRef}
+          flatListIndex={flatListIndex}
+          dataLength={data.length}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
 
@@ -32,7 +154,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#663399',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-})
+  itemContainer: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#F8E9B0',
+  },
+  itemTitle: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: 'black',
+  },
+  itemText: {
+    textAlign: 'center',
+    marginHorizontal: 35,
+    color: 'black',
+    lineHeight: 20,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    paddingVertical: 20,
+  },
+});
