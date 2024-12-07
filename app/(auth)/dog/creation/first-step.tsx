c324e2d0ed1ffccfd0d9060f15a64651d7bec35b
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-12-07 10:44:10 +0100

    feat(auth): add first step of dog creation flow
    This commit introduces the first step of the dog creation flow. It adds a new screen called `FirstStep` that displays a simple "First Step" message. This is the initial step in the dog creation process, which will be expanded upon in future commits.
    
    Additionally, this commit makes the following changes:
    
    - Adds the `useMMKVDevTools` hook to the `_layout.tsx` file, which enables MMKV development tools.
    - Updates the `_layout.tsx` file for the `(auth)/dog` route to include the new `creation` screen.
    - Adds a new `DogCreationLayout` component to handle the navigation for the dog creation flow.
    - Modifies the `account-created.tsx` file to include a button that navigates to the dog creation flow.
    - Updates the French and English translations to include the "Your account is created!" and "Create my dog profile" strings.

---
+++ app/(auth)/dog/creation/first-step.tsx
@@ -0,0 +1,10 @@
+import { Body } from "@/components/ui/text";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function FirstStep() {
+  return (
+    <SafeAreaView>
+      <Body>First Step</Body>
+    </SafeAreaView>
+  )
+}
