c3c963e401dd70447d19f4744c8e9d3e1435c723
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-12-09 08:57:23 +0100

    feat: Add image picker functionality to dog creation flow

--- app/(auth)/dog/creation/third-step.tsx
+++ app/(auth)/dog/creation/third-step.tsx
@@ -1,11 +1,32 @@
 import { i18n } from "@/app/_layout";
 import Back from "@/components/back-button";
 import BodyTitle from "@/components/bodyTitle/body-title";
-import { StyleSheet, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { Image } from "expo-image";
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from "react";
+import { Pressable, StyleSheet, View } from "react-native";
 import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
-
 export default function ThirdStep() {
   const insets = useSafeAreaInsets();
+
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images', 'videos'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -13,6 +34,31 @@ export default function ThirdStep() {
       <View style={[styles.content, { marginTop: insets.top }]}>
         <View style={styles.titleContainer}>
           <BodyTitle title={i18n.t('dogAddPhoto')} />
+          <Pressable onPress={pickImage}>
+            <View style={{
+              width: 150,
+              height: 150,
+              backgroundColor: '#C5C3C3',
+              borderRadius: 20,
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+              alignSelf: 'center',
+            }}>
+              <Image source={{ uri: image }} style={styles.image} />
+              <View style={{
+                width: 50,
+                height: 50,
+                backgroundColor: '#F7A400',
+                borderRadius: 999,
+                justifyContent: 'center',
+                alignItems: 'center',
+                position: 'relative',
+                bottom: -20,
+              }}>
+                <AntDesign name="plus" size={24} color="white" />
+              </View>
+            </View>
+          </Pressable>
         </View>
       </View>
     </SafeAreaView>
@@ -26,9 +72,17 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16
   },
   titleContainer: {
-    gap: 16
+    gap: 56
   },
   content: {
     gap: 20
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 20,
+    position: 'absolute',
+    top: 0,
+    left: 0,
   },
 });
