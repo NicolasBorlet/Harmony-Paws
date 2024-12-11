93107ebb6707d2f42321ffd55d745ffe6402d598
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-12-11 09:30:20 +0100

    feat(app): integrate react-query and update user data

--- components/dogListing/dog-listing.tsx
+++ components/dogListing/dog-listing.tsx
@@ -1,7 +1,8 @@
 import { Dog, DogDominance, DogSex } from '@/lib/api/types'
+import { session$ } from '@/lib/observables/session-observable'
 import { FlashList } from '@shopify/flash-list'
 import { router } from 'expo-router'
-import { useCallback, useMemo } from 'react'
+import { useCallback, useEffect, useMemo } from 'react'
 import { Pressable, View } from 'react-native'
 import OpacityFadeIn from '../animate/opacity-fadeIn'
 import DogItemListing from './dog-item-listing'
@@ -30,12 +31,12 @@ export default function DogListing() {
   // const dogs = dogs$.get()
 
   const separatorStyle = useMemo(() => ({ height: 20 }), [])
-  const contentContainerStyle = useMemo(() => ({ 
-    paddingHorizontal: 16, 
-    paddingTop: 24 
+  const contentContainerStyle = useMemo(() => ({
+    paddingHorizontal: 16,
+    paddingTop: 24
   }), [])
 
-  const handleDogPress = useCallback((dogId: string) => {
+  const handleDogPress = useCallback((dogId: number) => {
     router.push(`/dog/${dogId}`)
   }, [])
 
@@ -46,6 +47,11 @@ export default function DogListing() {
       </Pressable>
     </OpacityFadeIn>
   ), [handleDogPress])
+
+  useEffect(() => {
+    console.log('session', session$.get().id)
+    console.log('dogs', dogs)
+  }, [dogs])
 
   return (
     <FlashList
