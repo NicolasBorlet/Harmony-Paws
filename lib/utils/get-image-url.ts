ad4e8f0386d08ee6048ac3f1ba35a5f2e6e4f526
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-12-13 09:16:27 +0100

    feat: Update Supabase URL in .env and utils

--- lib/utils/get-image-url.ts
+++ lib/utils/get-image-url.ts
@@ -1,7 +1,7 @@
 import { session$ } from "../observables/session-observable";
 
 export const getImageUrl = async (dogId: string) => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/functions/v1/get-dog-image`, {
+  const response = await fetch(`${process.env.EXPO_PUBLIC_SUPABASE_URL}/functions/v1/get-dog-image`, {
     method: 'POST',
     headers: {
       'Authorization': `Bearer ${session$.get().access_token}`,
