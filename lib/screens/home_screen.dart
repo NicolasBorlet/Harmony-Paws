c5b24b20b4e3d49a5ade88b09b06e3d68c34fa29
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-01-10 14:03:53 +0100

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to HomeScreen!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
