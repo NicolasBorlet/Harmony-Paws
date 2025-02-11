106c6272efd28a668193de06fe747626414c8b73
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-02-11 11:40:33 +0100

    Refactor FormationDetails component by removing unnecessary GestureHandlerRootView and simplifying layout for improved readability and performance

--- app/(auth)/(tabs)/(formation)/[id].tsx
+++ app/(auth)/(tabs)/(formation)/[id].tsx
@@ -20,9 +20,7 @@ import { useLocalSearchParams } from 'expo-router'
 import { useCallback, useState } from 'react'
 import { ScrollView, View } from 'react-native'
 import {
-  Gesture,
-  GestureDetector,
-  GestureHandlerRootView,
+  Gesture
 } from 'react-native-gesture-handler'
 import { runOnJS, useSharedValue } from 'react-native-reanimated'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
@@ -65,9 +63,6 @@ export default function FormationDetails() {
     <>
       <Back />
       <ParallaxScrollView headerImage={formation.image || ''} paddingHorizontal={0}>
-        <GestureHandlerRootView
-        style={{ flex: 1, backgroundColor: 'white' }}
-        >
         <View
           style={{
             flex: 1,
@@ -93,81 +88,78 @@ export default function FormationDetails() {
             language={i18n.locale as 'fr' | 'en'}
           />
           <View style={{ flex: 1, paddingHorizontal: 16 }}>
-            <GestureDetector gesture={gesture}>
-              <View style={{ flex: 1 }}>
-                {selectedTab === 'about' ? (
-                  <ScrollView
-                    showsHorizontalScrollIndicator={false}
-                    contentContainerStyle={{
-                      gap: 20,
+            <View style={{ flex: 1 }}>
+              {selectedTab === 'about' ? (
+                <ScrollView
+                  showsHorizontalScrollIndicator={false}
+                  contentContainerStyle={{
+                    gap: 20,
+                  }}
+                >
+                  <View
+                    style={{
+                      marginTop: 20,
+                      display: 'flex',
+                      flexDirection: 'column',
+                      gap: 10,
+                      backgroundColor: 'rgba(102, 51, 153, 0.1)',
+                      borderRadius: 10,
+                      padding: 16,
+                      justifyContent: 'center',
                     }}
                   >
                     <View
                       style={{
-                        marginTop: 20,
                         display: 'flex',
-                        flexDirection: 'column',
-                        gap: 10,
-                        backgroundColor: 'rgba(102, 51, 153, 0.1)',
-                        borderRadius: 10,
-                        padding: 16,
-                        justifyContent: 'center',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
                       }}
                     >
+                      <BodyBold color={Colors.light.secondary}>
+                        {i18n.t('completeFormation')}
+                      </BodyBold>
                       <View
                         style={{
                           display: 'flex',
                           flexDirection: 'row',
-                          justifyContent: 'space-between',
+                          gap: 4,
+                          alignItems: 'center',
                         }}
                       >
-                        <BodyBold color={Colors.light.secondary}>
-                          {i18n.t('completeFormation')}
-                        </BodyBold>
-                        <View
-                          style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            gap: 4,
-                            alignItems: 'center',
-                          }}
-                        >
-                          <ModulePrice color='#F7A400'>
-                            {formation.price}€
-                          </ModulePrice>
-                          {formation.old_price && (
-                            <>
-                              <AntDesign name='arrowleft' size={12} color='black' />
-                              <ModulePrice color={Colors.light.secondary}>
-                                {formation.old_price}€
-                              </ModulePrice>
-                            </>
-                          )}
-                        </View>
-                      </View>
-                      <View>
-                        <ExtraSmallMedium color='#616060'>
-                          {formation.description}
-                        </ExtraSmallMedium>
+                        <ModulePrice color='#F7A400'>
+                          {formation.price}€
+                        </ModulePrice>
+                        {formation.old_price && (
+                          <>
+                            <AntDesign name='arrowleft' size={12} color='black' />
+                            <ModulePrice color={Colors.light.secondary}>
+                              {formation.old_price}€
+                            </ModulePrice>
+                          </>
+                        )}
                       </View>
                     </View>
-                    <ModuleListing modules={formation.modules || []} />
-                  </ScrollView>
-                ) : (
-                  <ScrollView
-                    showsVerticalScrollIndicator={false}
-                    contentContainerStyle={{
-                      gap: 20,
-                    }}
-                  >
-                    <AdviceListing />
-                  </ScrollView>
-                )}
-              </View>
-            </GestureDetector>
+                    <View>
+                      <ExtraSmallMedium color='#616060'>
+                        {formation.description}
+                      </ExtraSmallMedium>
+                    </View>
+                  </View>
+                  <ModuleListing modules={formation.modules || []} />
+                </ScrollView>
+              ) : (
+                <ScrollView
+                  showsVerticalScrollIndicator={false}
+                  contentContainerStyle={{
+                    gap: 20,
+                  }}
+                >
+                  <AdviceListing />
+                </ScrollView>
+              )}
+            </View>
           </View>
         </View>
-        </GestureHandlerRootView>
       </ParallaxScrollView>
     </>
   )
