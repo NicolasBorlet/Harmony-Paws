0179a2152f9e9686eff80e1efe534e7c0c11514b
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-02-14 14:58:09 +0100

    feat: Add title in card and black background with 0.2 opacity

--- components/formationListing/formation-listing-item.tsx
+++ components/formationListing/formation-listing-item.tsx
@@ -1,35 +1,62 @@
-import { Formation } from "@/lib/api/types";
-import { Image } from "expo-image";
-import { router } from "expo-router";
-import { Pressable } from "react-native";
-import Block from "../grid/Block";
-import { BodyBold, SmallMedium } from "../ui/text";
+import { Formation } from '@/lib/api/types'
+import { Image } from 'expo-image'
+import { router } from 'expo-router'
+import { Pressable, View } from 'react-native'
+import Block from '../grid/Block'
+import { BodyBold } from '../ui/text'
 
-const blurhash =
-  'L6Pj0^jE.AyE_3t7t7R**0o#DgR4';
+const blurhash = 'L6Pj0^jE.AyE_3t7t7R**0o#DgR4'
 
-export default function FormationListingItem({ formation }: { formation: Formation }) {
+export default function FormationListingItem({
+  formation,
+}: {
+  formation: Formation
+}) {
   return (
     <Pressable
-      onPress={()=>{
-        router.push(`/(formation)/${formation.id}`);
+      onPress={() => {
+        router.push(`/(formation)/${formation.id}`)
       }}
     >
-      <Block gap={8}>
-        <Block style={{
+      <Block
+        style={{
           width: '100%',
           height: 150,
           borderRadius: 20,
           overflow: 'hidden',
-        }}>
-          <Image source={{ uri: formation.image }} style={{
+          position: 'relative',
+        }}
+      >
+        <View
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+            zIndex: 1,
+          }}
+        />
+        <Image
+          source={{ uri: formation.image }}
+          style={{
+            position: 'absolute',
             height: '100%',
             width: '100%',
-          }} placeholder={{ blurhash }} />
-        </Block>
-        <BodyBold color="#F7A400">{formation.name}</BodyBold>
-        <SmallMedium color="#979898">{formation.animator_name}</SmallMedium>
+          }}
+          placeholder={{ blurhash }}
+        />
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            padding: 12,
+            zIndex: 2,
+          }}
+        >
+          <BodyBold color='#FFFFFF'>{formation.name}</BodyBold>
+        </View>
       </Block>
     </Pressable>
-  );
+  )
 }
