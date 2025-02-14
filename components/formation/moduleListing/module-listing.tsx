e31690c40ae93df787bf75ff5edb1fc2adfa2771
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-02-14 21:15:31 +0100

    feat: Text improvement & card stylization

--- components/formation/moduleListing/module-listing.tsx
+++ components/formation/moduleListing/module-listing.tsx
@@ -1,12 +1,9 @@
-import { ColumnItem } from "@/components/ui/ColumnItem";
-import { ModuleInterface } from "@/lib/api/types/interfaces";
-import { FlashList } from "@shopify/flash-list";
-import { router } from "expo-router";
-import { Pressable, View } from "react-native";
-import OpacityFadeIn from "../../animate/opacity-fadeIn";
-import ModuleListingItem from "./module-listing-item";
-
-const numColumns = 2;
+import { ModuleInterface } from '@/lib/api/types/interfaces'
+import { FlashList } from '@shopify/flash-list'
+import { router } from 'expo-router'
+import { Pressable, View } from 'react-native'
+import OpacityFadeIn from '../../animate/opacity-fadeIn'
+import ModuleListingItem from './module-listing-item'
 
 interface ModuleListingProps {
   modules?: ModuleInterface[]
@@ -16,25 +13,16 @@ export default function ModuleListing({ modules = [] }: ModuleListingProps) {
   return (
     <FlashList
       data={modules}
-      numColumns={numColumns}
       renderItem={({ index, item }) => (
-        <ColumnItem index={index} numColumns={numColumns}>
-          <Pressable onPress={() => router.push(`/module/${item.id}`)} style={{
-            flex: 1,
-            width: "100%",
-            height: "100%",
-          }}>
-            <OpacityFadeIn delay={index * 200}>
-              <ModuleListingItem module={item} />
-            </OpacityFadeIn>
-          </Pressable>
-        </ColumnItem>
+        <Pressable onPress={() => router.push(`/module/${item.id}`)}>
+          <OpacityFadeIn delay={index * 200}>
+            <ModuleListingItem module={item} />
+          </OpacityFadeIn>
+        </Pressable>
       )}
-      keyExtractor={(item) => item.id.toString()}
+      keyExtractor={item => item.id.toString()}
       scrollEnabled={false}
-      ItemSeparatorComponent={() => (
-        <View style={{ height: 16 }} />
-      )}
+      ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
       estimatedItemSize={10}
     />
   )
