f4bb3aca27a7f3f97a1e8b1b10041506fda74c74
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-02-15 16:35:22 +0100

    feat: Test custom checkbox

--- app/(auth)/dog/creation.tsx
+++ app/(auth)/dog/creation.tsx
@@ -1,16 +1,21 @@
 import { i18n } from '@/app/_layout'
 import Back from '@/components/back-button'
+import { Checkbox } from '@/components/checkbox'
 import ParallaxScrollViewText from '@/components/parallax-scrollview-text'
 import { ParagraphMedium, SpecialTitle_3 } from '@/components/ui/text'
 import { Colors } from '@/constants/Colors'
 import { useNavigation } from 'expo-router'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 export default function FirstStep() {
   const insets = useSafeAreaInsets()
   const navigation = useNavigation()
+
+  const [checked, setChecked] = useState(false)
+  const [checked2, setChecked2] = useState(false)
+  const [checked3, setChecked3] = useState(false)
 
   const canGoBack = navigation.canGoBack()
 
@@ -44,9 +49,26 @@ export default function FirstStep() {
         }
         backgroundColor={Colors.light.primary}
       >
-        <View style={{ backgroundColor: 'red' }}>
+        <View>
           <Text>Etape 1/2</Text>
           <Text>Informations</Text>
+          <View style={styles.listContainer}>
+            <Checkbox
+              label='test'
+              checked={checked}
+              onPress={() => setChecked(!checked)}
+            />
+            <Checkbox
+              label='test'
+              checked={checked2}
+              onPress={() => setChecked2(!checked2)}
+            />
+            <Checkbox
+              label='testtesttesttesttesttest'
+              checked={checked3}
+              onPress={() => setChecked3(!checked3)}
+            />
+          </View>
         </View>
       </ParallaxScrollViewText>
     </>
@@ -72,5 +94,12 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     paddingHorizontal: 20,
     gap: 20,
+  },
+  listContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 14,
+    marginTop: 24,
+    paddingRight: 16,
   },
 })
