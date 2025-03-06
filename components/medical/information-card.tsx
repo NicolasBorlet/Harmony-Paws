8addc777f0cd55c5c6167282de6eec5c7ca72c26
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-03-06 11:30:50 +0100

    feat: Create reusable InformationCard component for health record details

---
+++ components/medical/information-card.tsx
@@ -0,0 +1,59 @@
+import { Colors } from '@/constants/Colors'
+import { AntDesign } from '@expo/vector-icons'
+import { View } from 'react-native'
+import { ExtraSmallBold, ExtraSmallMedium } from '../ui/text'
+
+export default function InformationCard({
+  cardTitle,
+  cardIcon,
+  data,
+}: {
+  cardTitle: string
+  cardIcon: React.ReactNode
+  data: {
+    icon: React.ReactNode
+    title: string
+    date: string
+  }[]
+}) {
+  return (
+    <View
+      style={{
+        boxShadow: '0px 0px 13px 0px rgba(0, 0, 0, 0.1)',
+        borderRadius: 10,
+        paddingHorizontal: 12,
+        paddingVertical: 16,
+        gap: 12,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <View style={{ flexDirection: 'row', gap: 12, alignItems: 'center' }}>
+          {cardIcon}
+          <ExtraSmallBold color={Colors.purple[500]}>
+            {cardTitle}
+          </ExtraSmallBold>
+        </View>
+        <AntDesign name='right' size={12} color={Colors.purple[500]} />
+      </View>
+      {data.map((item, index) => (
+        <View style={{ gap: 8 }} key={index}>
+          <View style={{ flexDirection: 'row', gap: 4 }}>
+            <ExtraSmallBold color={Colors.pink[500]}>
+              {item.title}
+            </ExtraSmallBold>
+            <ExtraSmallMedium color={Colors.purple[500]}>-</ExtraSmallMedium>
+            <ExtraSmallMedium color={Colors.purple[500]}>
+              {item.date}
+            </ExtraSmallMedium>
+          </View>
+        </View>
+      ))}
+    </View>
+  )
+}
