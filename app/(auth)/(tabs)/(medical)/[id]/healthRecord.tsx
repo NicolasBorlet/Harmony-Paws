8362f76a38d87f79735c7289552d0fd037ba1721
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-03-06 11:22:53 +0100

    feat: Add vaccine section with icon and details in health record screen

--- app/(auth)/(tabs)/(medical)/[id]/healthRecord.tsx
+++ app/(auth)/(tabs)/(medical)/[id]/healthRecord.tsx
@@ -1,6 +1,7 @@
 import Back from '@/components/back-button'
-import { Body } from '@/components/ui/text'
+import { ExtraSmallBold, ExtraSmallMedium } from '@/components/ui/text'
 import { Colors } from '@/constants/Colors'
+import { AntDesign, MaterialIcons } from '@expo/vector-icons'
 import { useLocalSearchParams, usePathname } from 'expo-router'
 import { useEffect } from 'react'
 import { StyleSheet, View } from 'react-native'
@@ -19,7 +20,44 @@ export default function HealthRecord() {
   return (
     <View style={styles.container}>
       <Back position='relative' top={insets.top} left='0' />
-      <Body>Health Record</Body>
+      <View
+        style={{
+          boxShadow: '0px 0px 13px 0px rgba(0, 0, 0, 0.1)',
+          borderRadius: 10,
+          paddingHorizontal: 12,
+          paddingVertical: 16,
+          gap: 12,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <View style={{ flexDirection: 'row', gap: 12, alignItems: 'center' }}>
+            <MaterialIcons
+              name='vaccines'
+              size={24}
+              color={Colors.purple[500]}
+            />
+            <ExtraSmallBold color={Colors.purple[500]}>Vaccines</ExtraSmallBold>
+          </View>
+          <AntDesign name='right' size={12} color={Colors.purple[500]} />
+        </View>
+        <View style={{ gap: 8 }}>
+          <View style={{ flexDirection: 'row', gap: 4 }}>
+            <ExtraSmallBold color={Colors.pink[500]}>
+              La toux des chenils
+            </ExtraSmallBold>
+            <ExtraSmallMedium color={Colors.purple[500]}>-</ExtraSmallMedium>
+            <ExtraSmallMedium color={Colors.purple[500]}>
+              12 février 2025 (première injection)
+            </ExtraSmallMedium>
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
