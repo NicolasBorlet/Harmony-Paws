113e6f322a8ac1406f18b218d18f7af473c7890f
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-03-06 14:56:24 +0100

    refactor: Optimize header and scroll animations with Reanimated improvements

--- app/(auth)/(tabs)/(medical)/medical.tsx
+++ app/(auth)/(tabs)/(medical)/medical.tsx
@@ -9,16 +9,20 @@ import { DogListingInterface } from '@/lib/api/types'
 import { user$ } from '@/lib/observables/session-observable'
 import { FlashList } from '@shopify/flash-list'
 import { router } from 'expo-router'
+
 import { useEffect, useRef, useState } from 'react'
 import {
+  Animated,
   Dimensions,
   NativeScrollEvent,
   NativeSyntheticEvent,
   StyleSheet,
   View,
 } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler'
-import { useSharedValue } from 'react-native-reanimated'
+import {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from 'react-native-reanimated'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const CARD_WIDTH = 250 // Largeur de la carte
@@ -38,6 +42,12 @@ export default function Medical() {
 
   const user = user$.get()
   const { data: userDogs } = useDogsFromUserId(user?.id)
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: event => {
+      scrollY.value = event.contentOffset.y
+    },
+  })
 
   const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
     const scrollPosition = event.nativeEvent.contentOffset.x
@@ -89,11 +99,9 @@ export default function Medical() {
       <View style={{ paddingHorizontal: 16 }}>
         <MedicalHeader scrollY={scrollY} />
       </View>
-      <ScrollView
-        contentContainerStyle={styles.scrollView}
-        onScroll={event => {
-          scrollY.value = event.nativeEvent.contentOffset.y
-        }}
+      <Animated.ScrollView
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
         showsVerticalScrollIndicator={false}
       >
         <View style={{ padding: 16, paddingTop: 0 }}>
@@ -142,7 +150,7 @@ export default function Medical() {
             <Body color={Colors.white}>{i18n.t('global.choose')}</Body>
           </StandardButton>
         </View>
-      </ScrollView>
+      </Animated.ScrollView>
     </View>
   )
 }
@@ -151,11 +159,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-  },
-  scrollView: {
-    paddingHorizontal: 16,
-    gap: 32,
-    paddingBottom: 24,
   },
   buttonContainer: {
     padding: 16,
