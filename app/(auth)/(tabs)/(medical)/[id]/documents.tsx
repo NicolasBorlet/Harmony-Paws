f76f66a8224dc0f2da878374abed388f8d1fbe95
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-03-07 13:27:22 +0100

    feat: Add detailed medical record navigation and localization

---
+++ app/(auth)/(tabs)/(medical)/[id]/documents.tsx
@@ -0,0 +1,10 @@
+import { Body } from '@/components/ui/text'
+import { View } from 'react-native'
+
+export default function Documents() {
+  return (
+    <View>
+      <Body>Documents</Body>
+    </View>
+  )
+}
