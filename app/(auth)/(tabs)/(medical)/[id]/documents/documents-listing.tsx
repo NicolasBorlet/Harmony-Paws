bb719499289bf2ca6ac8397801c9b8200b8e36f0
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-03-08 14:53:01 +0100

    feat: Add month selector and document upload button to documents listing

--- app/(auth)/(tabs)/(medical)/[id]/documents/documents-listing.tsx
+++ app/(auth)/(tabs)/(medical)/[id]/documents/documents-listing.tsx
@@ -2,9 +2,10 @@ import { i18n } from '@/app/_layout'
 import Back from '@/components/back-button'
 import ItemListing from '@/components/document/item-listing'
 import Divider from '@/components/ui/divider'
-import { BodyBold } from '@/components/ui/text'
+import { BodyBold, SpecialTitle_3 } from '@/components/ui/text'
 import { Colors } from '@/constants/Colors'
 import { useDogDocuments } from '@/lib/api/dog'
+import { AntDesign, Entypo } from '@expo/vector-icons'
 import { router, useLocalSearchParams } from 'expo-router'
 import { Linking, Platform, Pressable, StyleSheet, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
@@ -26,6 +27,40 @@ export default function Documents() {
         {i18n.t('documents.myDocuments')}
       </BodyBold>
       <Divider spacing={32} />
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginBottom: 32,
+        }}
+      >
+        <View
+          style={{
+            gap: 8,
+            flexDirection: 'row',
+            alignItems: 'center',
+            flex: 1,
+          }}
+        >
+          <SpecialTitle_3>Mars 2025</SpecialTitle_3>
+          <Entypo name='chevron-down' size={16} color={Colors.purple[500]} />
+        </View>
+        <Pressable
+          style={{
+            backgroundColor: Colors.orange[500],
+            padding: 8,
+            borderRadius: 16,
+            width: 32,
+            height: 32,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <AntDesign name='plus' size={16} color={Colors.white} />
+        </Pressable>
+      </View>
       {documents?.map(document => (
         <Pressable
           key={document.name}
