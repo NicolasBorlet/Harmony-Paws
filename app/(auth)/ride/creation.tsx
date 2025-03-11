0a8a53dfda27f6871727d11af6023e521b682c3e
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-03-11 13:39:03 +0100

    feat: Add ride creation screen layout and basic content

--- app/(auth)/ride/creation.tsx
+++ app/(auth)/ride/creation.tsx
@@ -1,7 +1,12 @@
+import { Body } from '@/components/ui/text'
 import { StyleSheet, View } from 'react-native'
 
 export default function RideCreation() {
-  return <View style={styles.container}></View>
+  return (
+    <View style={styles.container}>
+      <Body>Ride Creation</Body>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
