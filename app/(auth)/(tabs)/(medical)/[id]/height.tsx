849475f1083b1a5ad18b9ab42bbba00593d90a98
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-03-18 13:32:56 +0100

    feat: Add tooltip functionality and improve chart interaction in Height component

--- app/(auth)/(tabs)/(medical)/[id]/height.tsx
+++ app/(auth)/(tabs)/(medical)/[id]/height.tsx
@@ -1,12 +1,14 @@
 import { i18n } from '@/app/_layout'
 import Back from '@/components/back-button'
+import Divider from '@/components/ui/divider'
 import { BodyBold } from '@/components/ui/text'
 import { Colors } from '@/constants/Colors'
 import { Montserrat_400Regular } from '@expo-google-fonts/montserrat'
-import { useFont } from '@shopify/react-native-skia'
+import { Circle, useFont } from '@shopify/react-native-skia'
 import { ScrollView, View } from 'react-native'
+import type { SharedValue } from 'react-native-reanimated'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { CartesianChart, Line } from 'victory-native'
+import { CartesianChart, Line, useChartPressState } from 'victory-native'
 
 const DATA = [
   { x: 1, y: 2 },
@@ -20,56 +22,71 @@ const DATA = [
 export default function Height() {
   const insets = useSafeAreaInsets()
   const font = useFont(Montserrat_400Regular, 12)
+  const { state, isActive } = useChartPressState({ x: 0, y: { highTmp: 0 } })
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Back position='relative' top={insets.top} left='0' />
-      <BodyBold
-        style={{
-          textAlign: 'center',
-          marginTop: 8,
-        }}
-      >
-        {i18n.t('height.heightTracking')}
-      </BodyBold>
-      <View style={{ flex: 1 }}>
-        <View
+    <View style={{ flex: 1, backgroundColor: Colors.white }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Back position='relative' top={insets.top} left='0' />
+        <BodyBold
           style={{
-            backgroundColor: '#F0EBF5',
-            padding: 12,
-            borderRadius: 8,
-            height: 300,
+            textAlign: 'center',
+            marginTop: 8,
           }}
         >
-          <CartesianChart
-            data={DATA}
-            xKey='x'
-            yKeys={['y']}
-            axisOptions={{
-              font: font,
+          {i18n.t('height.heightTracking')}
+        </BodyBold>
+        <Divider spacing={32} />
+        <View style={{ flex: 1 }}>
+          <View
+            style={{
+              backgroundColor: '#F0EBF5',
+              padding: 12,
+              borderRadius: 8,
+              height: 300,
             }}
           >
-            {({ points }) => (
-              //👇 pass a PointsArray to the Line component, as well as options.
-              <Line
-                points={points.y}
-                color={Colors.purple[500]}
-                strokeWidth={2}
-                animate={{ type: 'timing', duration: 300 }}
-                curveType='natural'
-              />
-            )}
-          </CartesianChart>
+            <CartesianChart
+              data={DATA}
+              xKey='x'
+              yKeys={['y']}
+              axisOptions={{
+                font: font,
+              }}
+            >
+              {({ points }) => (
+                <>
+                  {/* Pass a PointsArray to the Line component, as well as options */}
+                  <Line
+                    points={points.y}
+                    color={Colors.purple[500]}
+                    strokeWidth={2}
+                    animate={{ type: 'timing', duration: 300 }}
+                    curveType='natural'
+                  />
+                  {isActive && (
+                    <ToolTip
+                      x={state.x.position}
+                      y={state.y.highTmp.position}
+                    />
+                  )}
+                </>
+              )}
+            </CartesianChart>
+          </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </View>
   )
+}
+
+function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
+  return <Circle cx={x} cy={y} r={8} color='black' />
 }
 
 const styles = {
   container: {
     flex: 1,
-    backgroundColor: Colors.white,
     paddingHorizontal: 16,
   },
 }
