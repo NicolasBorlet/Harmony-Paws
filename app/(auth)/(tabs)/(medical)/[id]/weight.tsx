bbb90545d8fd63b5ea1f53852374aca7c29adadb
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-03-29 11:56:52 +0100

    feat: Refactor Weight component to utilize FlatList for rendering

--- app/(auth)/(tabs)/(medical)/[id]/weight.tsx
+++ app/(auth)/(tabs)/(medical)/[id]/weight.tsx
@@ -12,7 +12,7 @@ import { AntDesign } from '@expo/vector-icons'
 import { Circle, useFont } from '@shopify/react-native-skia'
 import { useLocalSearchParams } from 'expo-router'
 import React, { memo, useCallback, useEffect, useMemo } from 'react'
-import { Pressable, ScrollView, View } from 'react-native'
+import { Pressable, View } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
 import type { SharedValue } from 'react-native-reanimated'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
@@ -128,115 +128,121 @@ export default function Weight() {
 
   return (
     <View style={{ flex: 1, backgroundColor: Colors.white }}>
-      <ScrollView contentContainerStyle={styles.container}>
-        <Back position='relative' top={insets.top} left='0' />
-        <BodyBold
-          style={{
-            textAlign: 'center',
-            marginTop: 8,
-          }}
-        >
-          {i18n.t('weight.weightTracking')}
-        </BodyBold>
-        <Divider spacing={32} />
-        <View
-          style={{
-            backgroundColor: '#F0EBF5',
-            padding: 12,
-            borderRadius: 8,
-            height: 300,
-          }}
-        >
-          <CartesianChart
-            data={formattedData}
-            xKey='x'
-            yKeys={yKeysToUse}
-            axisOptions={{
-              font: font,
-              formatXLabel: value => {
-                const date = new Date(value)
-                return date.toLocaleDateString('fr-FR', {
-                  month: 'short',
-                  year: 'numeric',
-                })
-              },
-            }}
-          >
-            {({ points }) => (
-              <>
-                {selectedYears.map((year, index) => {
-                  const yKey = `y${index + 1}` as YearKey
-                  return (
-                    <Line
-                      key={year}
-                      points={points[yKey]}
-                      color={yearColors[yKey]}
-                      strokeWidth={2}
-                      animate={{ type: 'timing', duration: 300 }}
-                      curveType='natural'
-                    />
-                  )
-                })}
-                {isActive && (
-                  <ToolTip x={state.x.position} y={state.y.highTmp.position} />
+      <FlatList
+        data={weightMeasurements}
+        ListHeaderComponent={() => (
+          <>
+            <Back position='relative' top={insets.top} left='0' />
+            <BodyBold
+              style={{
+                textAlign: 'center',
+                marginTop: 8,
+              }}
+            >
+              {i18n.t('weight.weightTracking')}
+            </BodyBold>
+            <Divider spacing={32} />
+            <View
+              style={{
+                backgroundColor: '#F0EBF5',
+                padding: 12,
+                borderRadius: 8,
+                height: 300,
+              }}
+            >
+              <CartesianChart
+                data={formattedData}
+                xKey='x'
+                yKeys={yKeysToUse}
+                axisOptions={{
+                  font: font,
+                  formatXLabel: value => {
+                    const date = new Date(value)
+                    return date.toLocaleDateString('fr-FR', {
+                      month: 'short',
+                      year: 'numeric',
+                    })
+                  },
+                }}
+              >
+                {({ points }) => (
+                  <>
+                    {selectedYears.map((year, index) => {
+                      const yKey = `y${index + 1}` as YearKey
+                      return (
+                        <Line
+                          key={year}
+                          points={points[yKey]}
+                          color={yearColors[yKey]}
+                          strokeWidth={2}
+                          animate={{ type: 'timing', duration: 300 }}
+                          curveType='natural'
+                        />
+                      )
+                    })}
+                    {isActive && (
+                      <ToolTip
+                        x={state.x.position}
+                        y={state.y.highTmp.position}
+                      />
+                    )}
+                  </>
                 )}
-              </>
-            )}
-          </CartesianChart>
-        </View>
-        <Divider spacing={32} />
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginBottom: 24,
-          }}
-        >
-          <BodySemiBold color={Colors.purple[500]}>
-            {i18n.t('global.history')}
-          </BodySemiBold>
-          <FullRoundedButton height={48} width={48}>
-            <AntDesign name='plus' size={20} color='white' />
-          </FullRoundedButton>
-        </View>
-        <View style={{ flexDirection: 'row', gap: 12, marginBottom: 24 }}>
-          {YEAR.map(year => (
-            <YearSelector
-              key={year}
-              year={year}
-              isSelected={selectedYears.includes(year)}
-              onSelect={handleYearSelect}
-            />
-          ))}
-        </View>
-        <FlatList
-          data={weightMeasurements}
-          renderItem={({ item }) => (
+              </CartesianChart>
+            </View>
+            <Divider spacing={32} />
             <View
               style={{
                 flexDirection: 'row',
-                gap: 12,
-                padding: 8,
-                backgroundColor: `${Colors.pink[500]}3A`,
-                borderRadius: 8,
                 justifyContent: 'space-between',
+                alignItems: 'center',
+                marginBottom: 24,
               }}
             >
-              <Body>
-                {new Date(item.date).toLocaleDateString('fr-FR', {
-                  day: 'numeric',
-                  month: 'long',
-                  year: 'numeric',
-                })}
-              </Body>
-              <BodyBold>{item.weight} kg</BodyBold>
+              <BodySemiBold color={Colors.purple[500]}>
+                {i18n.t('global.history')}
+              </BodySemiBold>
+              <FullRoundedButton height={48} width={48}>
+                <AntDesign name='plus' size={20} color='white' />
+              </FullRoundedButton>
             </View>
-          )}
-          keyExtractor={item => item.date}
-          ItemSeparatorComponent={() => <View style={{ height: 8 }} />}
-        />
-      </ScrollView>
+            <View style={{ flexDirection: 'row', gap: 12, marginBottom: 24 }}>
+              {YEAR.map(year => (
+                <YearSelector
+                  key={year}
+                  year={year}
+                  isSelected={selectedYears.includes(year)}
+                  onSelect={handleYearSelect}
+                />
+              ))}
+            </View>
+          </>
+        )}
+        contentContainerStyle={[styles.container, { paddingBottom: 24 }]}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flexDirection: 'row',
+              gap: 12,
+              padding: 8,
+              backgroundColor: `${Colors.pink[500]}3A`,
+              borderRadius: 8,
+              justifyContent: 'space-between',
+            }}
+          >
+            <Body>
+              {new Date(item.date).toLocaleDateString('fr-FR', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric',
+              })}
+            </Body>
+            <BodyBold>{item.weight} kg</BodyBold>
+          </View>
+        )}
+        keyExtractor={item => item.date}
+        ItemSeparatorComponent={() => <View style={{ height: 8 }} />}
+      />
     </View>
   )
 }
