7bbcb798cd71b6a6aacb54c15d43e19105b8cdd9
Nicolas Borlet <nicolas.borlet7@gmail.com>
2025-06-04 08:59:25 +0200

    feat: Add new screens for weight, vaccines, and callback; implement Callback component for reminders

--- components/medical/health-record-header.tsx
+++ components/medical/health-record-header.tsx
@@ -7,9 +7,11 @@ import RoundedIconLink from '../rounded-icon-link'
 export default function HealthRecordHeader({
   scrollY,
   dogName,
+  dogId,
 }: {
   scrollY: any
   dogName: string
+  dogId: string
 }) {
   return (
     <AnimatedHeader
@@ -18,7 +20,9 @@ export default function HealthRecordHeader({
         <>
           <RoundedIconLink
             icon={<Ionicons name='notifications' size={20} color='white' />}
-            onPress={() => router.push('/notifications')}
+            onPress={() =>
+              router.push(`/(auth)/(tabs)/(medical)/${dogId}/callback`)
+            }
             color={Colors.pink[500]}
           />
           <RoundedIconLink
